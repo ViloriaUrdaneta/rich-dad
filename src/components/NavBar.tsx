3656207d2e5cfce00731d673790644fde36dd314
Miguel <miguel.viloria.247@gmail.com>
2023-11-15 15:07:46 -0300

    ui login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,24 +13,26 @@ function NavBar() {
         <div>
             <nav className='w-full shadow-md fixed top-0 left-0 right-0 z-10 h-20 align-middle border-b '>
                 <div className='flex justify-around align-middle mt-3.5 '>
-                    <div className='flex'>
-                        <Image src={rdIcon} alt="typescript" width={50} height={50} />
-                        <h1 className='text-3xl font-bold font-mono self-center ml-4'>Rich Dad</h1> 
-                    </div>
+                    <Link href={'/'}>
+                        <div className='flex'>
+                            <Image src={rdIcon} alt="typescript" width={50} height={50} />
+                            <h1 className='text-3xl font-bold font-mono self-center ml-4'>Rich Dad</h1> 
+                        </div>   
+                    </Link>
                     <div>
                         {session ? (
                             <>
-                                <button className='shadow-md hover:shadow-lg rounded-full border border-inherit h-11 w-36' onClick={() =>{signOut()}}>Log out</button>
+                                <button className='shadow-md hover:shadow-lg dark:hover:bg-slate-800 rounded-lg border border-inherit h-11 w-36' onClick={() =>{signOut()}}>Log out</button>
                             </>
                         ) : (
                             <>
                                 <Link href={'/register'}>
-                                    <button className='hover:bg-slate-200 dark:hover:bg-slate-800 rounded-full h-11 w-36 mr-2'>
+                                    <button className='hover:bg-slate-200 dark:hover:bg-slate-800 rounded-lg h-11 w-36 mr-2'>
                                         Sign up
                                     </button>
                                 </Link>
                                 <Link href={'/login'}>
-                                    <button className='shadow-md hover:shadow-lg hover:bg-slate-200  dark:hover:bg-slate-800 rounded-full border border-inherit h-11 w-36'>
+                                    <button className='shadow-md hover:shadow-lg hover:bg-slate-200 dark:hover:bg-slate-800 rounded-lg border border-inherit h-11 w-36'>
                                         Log in
                                     </button>
                                 </Link>
