bc4f9e35d8ae950094179e13c6a989d508e3eb47
Miguel <miguel.viloria.247@gmail.com>
2023-11-16 10:59:36 -0300

    sidebar

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function BalancePage() {
+export default function BookPage() {
     return (
         <div>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <h1 className='text-5xl font-light font-sans self-center ml-4 mt-8 text-center'>Balance</h1>
+                <h1 className='text-5xl font-light font-sans self-center ml-4 mt-8 text-center'>Book</h1>
             </main>
         </div>
     )
---
+++ src/app/dashboard/shoppinglist/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShoppingListPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className='text-5xl font-light font-sans self-center ml-4 mt-8 text-center'>Shopping List</h1>
+        </main>
+    )
+}
