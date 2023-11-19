5f82981d820311e094853352c8628bf3f54112c2
Miguel <miguel.viloria.247@gmail.com>
2023-11-19 00:52:43 -0300

    truncate

--- src/components/Notebook.tsx
+++ src/components/Notebook.tsx
@@ -14,9 +14,9 @@ const NotebookComp: React.FC<NotebookProps> = ({ notebooks }) => {
                 return (
                     <div key={nb.id}>
                         <div className='rounded-md border-l-8 border-l-black dark:border-l-white m-8 border-2 w-40 h-52 shadow-md flex flex-row bg-slate-200 hover:shadow-xl dark:bg-slate-950 dark:hover:bg-slate-900'>  
-                            <div className=''>
-                                <p className='font-bold text-lg text-left m-8'>{nb.name}</p>
-                            </div>
+                            
+                                <p className='font-bold text-lg text-left ml-6 mt-8 text-clip overflow-hidden'>{nb.name}</p>
+                            
                         </div>
                     </div>
                 )
